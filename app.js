e234622f265274e787719f56b5de11fdfeabf8dc
nusratSaba <1711075@iub.edu.bd>
2021-05-18 02:10:25 +0600

    update pages.js

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cookieParser());
 app.set('view engine', 'hbs');
-//let db = new sqlite3.Database('C:/Users/Asus/Desktop/Black cat/SPM_Advanced/DataSource/database.sqlite3', (err) => {
+let db = new sqlite3.Database('C:/Users/Asus/Desktop/Black cat/SPM_Advanced/DataSource/database.sqlite3', (err) => {
 //let db = new sqlite3.Database('/home/tahmid/Git/SPM_Advanced/DataSource/database.sqlite3', (err) => {
 //let db = new sqlite3.Database('E:/Spring 2021 course work/SPM-NEW/SPM_Advanced/DataSource/database.sqlite3', (err) => {
-let db = new sqlite3.Database('E:/SPM_Advanced/DataSource/database.sqlite3', (err) => {
+//let db = new sqlite3.Database('E:/SPM_Advanced/DataSource/database.sqlite3', (err) => {
     if (err) {
         return console.error(err.message);
     }
--- routes/pages.js
+++ routes/pages.js
@@ -2,8 +2,9 @@ const sqlite3 = require("sqlite3").verbose();
 const express = require("express");
 const Router = express.Router();
 let User = require('../controllers/auth');
+let db = new sqlite3.Database('C:/Users/Asus/Desktop/Black cat/SPM_Advanced/DataSource/database.sqlite3', (err) => {
 //let db = new sqlite3.Database('E:/Spring 2021 course work/SPM-NEW/SPM_Advanced/DataSource/database.sqlite3', (err) => {
-let db = new sqlite3.Database('E:/SPM_Advanced/DataSource/database.sqlite3', (err) => {
+//let db = new sqlite3.Database('E:/SPM_Advanced/DataSource/database.sqlite3', (err) => {
     if (err) {
         return console.error(err.message);
     }
@@ -96,7 +97,7 @@ Router.get("/Faculty", (req,res) => {
 });
 Router.get("/facultyCourses", (req,res) => {
 
-    res.render(`facultyCourses`, {FacultyID: User.FacultyID,  F_fname: User.F_fname, F_lName:User.F_lName, F_Gender:User.F_Gender, F_DateOfBirth:User.F_DateOfBirth, F_Email:User.F_Email, F_Phone:User.F_Phone,F_Address:User.F_Address, FacultyProfile:User.FacultyProfile, DepartmentID:User.DepartmentID, sem: User.semester.EnrolledSem})
+    res.render(`facultyCourses`, {FacultyID: User.FacultyID,  F_fname: User.F_fname, F_lName:User.F_lName, F_Gender:User.F_Gender, F_DateOfBirth:User.F_DateOfBirth, F_Email:User.F_Email, F_Phone:User.F_Phone,F_Address:User.F_Address, FacultyProfile:User.FacultyProfile, DepartmentID:User.DepartmentID })
 });
 Router.get("/facultyDataEntry", (req,res) => {
 
